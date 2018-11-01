4a26158de25e520f5c195133973c37c032d9935f
Aisling Guinan <oldAislingguinan@Aislings-MacBook-Pro.local>
2018-11-01 14:04:59 +0000

    Updates

---
+++ precache-manifest.f985a8c7970e0e60ae80a9625372e374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game-of-life/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "48c5f91bb349b88c84a7",
+    "url": "/game-of-life/static/js/runtime~main.48c5f91b.js"
+  },
+  {
+    "revision": "fee824a87112d4e529c5",
+    "url": "/game-of-life/static/js/main.fee824a8.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/game-of-life/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "fee824a87112d4e529c5",
+    "url": "/game-of-life/static/css/main.d007f1ef.chunk.css"
+  },
+  {
+    "revision": "06882638d530a5c1040b6a9698d182dd",
+    "url": "/game-of-life/index.html"
+  }
+];
